e791d8d570844848550de9f3db47584757df802e
Joan-Alexander Grigorov <sasquatch.zaclan@gmail.com>
2012-07-03 00:31:43 +0300

    Docblock added

--- src/galanthus/dispatcher/ResponseInterface.php
+++ src/galanthus/dispatcher/ResponseInterface.php
@@ -1,9 +1,28 @@
 <?php
+/**
+ * Galanthus Framework © 2012
+ * Copyright © 2012 Sasquatch <Joan-Alexander Grigorov>
+ *                              http://bgscripts.com
+ *
+ * LICENSE
+ *
+ * This source file is subject to the GNU General Public License v3
+ * that is bundled with this package in the file LICENSE.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @category   Galanthus
+ * @package    Galanthus Dispatcher
+ * @copyright  Copyright (c) 2012 Sasquatch, Elegance Team
+ */
 
 namespace galanthus\dispatcher;
 
 /**
- * @todo Parses the requested url and finds the requested controller and etc.
+ * @author     Joan-Alexander Grigorov http://bgscripts.com
+ * @category   Galanthus
+ * @package    Galanthus Dispatcher
+ * @copyright  Copyright (c) 2012 Sasquatch, Elegance Team
  */
 interface ResponseInterface
 {
