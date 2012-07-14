23fc3bcb9bb4af0de4aa53d7da2fd4690e68adf3
Joan-Alexander Grigorov <sasquatch.zaclan@gmail.com>
2012-07-14 14:16:15 +0300

    The vendor directory is added to the include path

--- configs/di/common.php
+++ configs/di/common.php
@@ -3,7 +3,7 @@ return array(
     'galanthus\env\config\IncludePath' => array(
         'params' => array(
             'paths' => array(
-                ROOT_PATH . '/vendor/doctrine/dbal/lib'
+                ROOT_PATH . '/vendor/'
             )
         )
     )
