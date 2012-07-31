0a5c931747aa0fdd1dc861a35f3bfc97e00bbadd
Joan-Alexander Grigorov <sasquatch.zaclan@gmail.com>
2012-07-31 23:27:19 +0300

    Default project structure is moved to another repository

--- public_html/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Define path to the root directory
-defined('ROOT_PATH')
-    || define('ROOT_PATH', realpath(dirname(__FILE__) . '/../'));
-
-require_once ROOT_PATH . '/configs/bootstrap.php';
