db1dc0a160a721f1073de6f737aade418297e964
Joan-Alexander Grigorov <sasquatch.zaclan@gmail.com>
2012-08-02 10:16:36 +0300

    DocBlock documentation added

--- src/Quasar/Entity/Arrayable.php
+++ src/Quasar/Entity/Arrayable.php
@@ -1,10 +1,37 @@
 <?php
+/**
+ * Quasar Framework © 2012
+ * Copyright © 2012 Sasquatch <Joan-Alexander Grigorov>
+ *                              http://bgscripts.com
+ *
+ * LICENSE
+ *
+ * This source file is subject to the GNU General Public License v3
+ * that is bundled with this package in the file LICENSE.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @category   Quasar
+ * @package    Quasar Entity
+ * @copyright  Copyright (c) 2012 Sasquatch
+ */
 
 namespace Quasar\Entity;
 
+/**
+ * @author     Joan-Alexander Grigorov http://bgscripts.com
+ * @category   Quasar
+ * @package    Quasar Entity
+ * @copyright  Copyright (c) 2012 Sasquatch
+ */
 interface Arrayable
 {
     
+    /**
+     * Get entity's data
+     *
+     * @return array
+     */
     public function toArray();
     
 }
--- src/Quasar/Entity/Standard.php
+++ src/Quasar/Entity/Standard.php
@@ -1,11 +1,29 @@
 <?php
+/**
+ * Quasar Framework © 2012
+ * Copyright © 2012 Sasquatch <Joan-Alexander Grigorov>
+ *                              http://bgscripts.com
+ *
+ * LICENSE
+ *
+ * This source file is subject to the GNU General Public License v3
+ * that is bundled with this package in the file LICENSE.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @category   Quasar
+ * @package    Quasar Entity
+ * @copyright  Copyright (c) 2012 Sasquatch
+ */
 
 namespace Quasar\Entity;
 
-use \ArrayAccess,
-    \IteratorAggregate,
-    \ArrayIterator;
-
+/**
+ * @author     Joan-Alexander Grigorov http://bgscripts.com
+ * @category   Quasar
+ * @package    Quasar Entity
+ * @copyright  Copyright (c) 2012 Sasquatch
+ */
 abstract class Standard implements Populatable, Arrayable
 {
     
@@ -38,4 +56,41 @@ abstract class Standard implements Populatable, Arrayable
     {
         return $this->data;
     }
+    
+    /**
+     * Overriding: allow property access through getter method
+     * 
+     * @param string $name
+     * @throws NoSuchFieldException
+     * @return mixed
+     */
+    public function __get($name)
+    {
+        $getter = 'get' . ucfirst($name);
+        
+        if (!method_exists($this, $getter)) {
+            throw new NoSuchFieldException("Specified field '$name' is not in the row");
+        }
+        
+        return $this->$getter();
+    }
+    
+    /**
+     * Overriding: allow property access through setter method
+     * 
+     * @param string $name
+     * @param mixed $value
+     * @throws NoSuchFieldException
+     */
+    public function __set($name, $value)
+    {
+        $setter = 'set' . ucfirst($name);
+        
+        if (!method_exists($this, $setter)) {
+            throw new NoSuchFieldException("Specified field '$name' is not in the row");
+        }
+        
+        $this->$setter($value);
+    }
+    
 }
